62db875f6c96c6bc92c3f36204a018bcd1411da2
Niek <niekmeg@gmail.com>
2022-08-14 21:07:29 +0200

    update to 1.19.2

--- src/main/java/com/reductos/frikandelbroodje/util/LootTableModifiers.java
+++ src/main/java/com/reductos/frikandelbroodje/util/LootTableModifiers.java
@@ -1,52 +1,40 @@
 package com.reductos.frikandelbroodje.util;
 
 import com.reductos.frikandelbroodje.init.ItemInit;
-import net.fabricmc.fabric.api.loot.v1.FabricLootPoolBuilder;
-import net.fabricmc.fabric.api.loot.v1.event.LootTableLoadingCallback;
-import net.minecraft.data.server.EntityLootTableGenerator;
-import net.minecraft.entity.EntityType;
-import net.minecraft.inventory.SimpleInventory;
-import net.minecraft.item.ItemStack;
-import net.minecraft.item.Items;
+import net.fabricmc.fabric.api.loot.v2.FabricLootPoolBuilder;
+import net.fabricmc.fabric.api.loot.v2.LootTableEvents;
 import net.minecraft.loot.LootPool;
-import net.minecraft.loot.LootTable;
 import net.minecraft.loot.condition.EntityPropertiesLootCondition;
 import net.minecraft.loot.condition.RandomChanceLootCondition;
 import net.minecraft.loot.context.LootContext;
 import net.minecraft.loot.entry.ItemEntry;
-import net.minecraft.loot.entry.LeafEntry;
-import net.minecraft.loot.entry.LootPoolEntry;
 import net.minecraft.loot.function.FurnaceSmeltLootFunction;
-import net.minecraft.loot.function.LootFunction;
 import net.minecraft.loot.function.LootingEnchantLootFunction;
 import net.minecraft.loot.function.SetCountLootFunction;
 import net.minecraft.loot.provider.number.ConstantLootNumberProvider;
 import net.minecraft.loot.provider.number.UniformLootNumberProvider;
 import net.minecraft.predicate.entity.EntityFlagsPredicate;
 import net.minecraft.predicate.entity.EntityPredicate;
-import net.minecraft.recipe.RecipeType;
-import net.minecraft.recipe.SmeltingRecipe;
 import net.minecraft.util.Identifier;
 
 public class LootTableModifiers {
-    private static final Identifier CREEPER_ID
+    private static final Identifier HORSE_ID
             = new Identifier("minecraft", "entities/horse");
     private static final EntityPredicate.Builder NEEDS_ENTITY_ON_FIRE = EntityPredicate.Builder.create().flags(EntityFlagsPredicate.Builder.create().onFire(true).build());
 
     public static void modifyLootTables() {
 
-        LootTableLoadingCallback.EVENT.register(((resourceManager, manager, id, supplier, setter) -> {
-            if (CREEPER_ID.equals(id)) {
-                FabricLootPoolBuilder poolBuilder = FabricLootPoolBuilder.builder()
+        LootTableEvents.MODIFY.register((resourceManager, manager, id, tableBuilder, setter) -> {
+            if (HORSE_ID.equals(id)) {
+                LootPool.Builder poolBuilder = LootPool.builder()
                         .rolls(ConstantLootNumberProvider.create(1))
                         .conditionally(RandomChanceLootCondition.builder(1f)) // Drops 100% of the time
-                        .with(ItemEntry.builder(ItemInit.HORSE_MEAT)
-                                .apply(FurnaceSmeltLootFunction.builder().conditionally(EntityPropertiesLootCondition.builder(LootContext.EntityTarget.THIS, NEEDS_ENTITY_ON_FIRE)))
-                        ).withFunction(SetCountLootFunction.builder(UniformLootNumberProvider.create(0.0f, 2.0f)).build())
-                        .withFunction(LootingEnchantLootFunction.builder(UniformLootNumberProvider.create(0.0F, 1.0F)).build());
-                supplier.withPool(poolBuilder.build());
+                        .with(ItemEntry.builder(ItemInit.HORSE_MEAT).apply(FurnaceSmeltLootFunction.builder().conditionally(EntityPropertiesLootCondition.builder(LootContext.EntityTarget.THIS, NEEDS_ENTITY_ON_FIRE))))
+                        .apply(SetCountLootFunction.builder(UniformLootNumberProvider.create(0.0f, 1.0f)).build())
+                        .apply(LootingEnchantLootFunction.builder(UniformLootNumberProvider.create(0.5F, 1.0F)).build());
+                tableBuilder.pool(poolBuilder.build());
             }
-        }));
-    }
+        });
 
+    }
 }
